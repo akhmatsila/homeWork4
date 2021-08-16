ce27d46b8c962b7b7c2bae820f0b6bad7ee08774
akhmatsila <44005477+akhmatsila@users.noreply.github.com>
2021-08-16 18:49:47 +0300

    Update ModelApi.java

--- app/src/main/java/com/example/indalamar/scp_1488/ModelApi.java
+++ app/src/main/java/com/example/indalamar/scp_1488/ModelApi.java
@@ -7,6 +7,6 @@ import retrofit2.http.GET;
 
 public interface ModelApi {
 
-    @GET("method/photos.get?owner_id=-53759180&album_id=wall&count=50&v=5.92&access_token=54548621624eeb2de6fc4e8057a8d05dddf63b78a364828f72d2a6e494563c0bfb1f381a16cfc1ff876f0")
+    @GET("method/photos.get?owner_id=-53759180&album_id=wall&count=50&v=5.92&access_token=???")
     Call<Model> getMyJSON();
 }
